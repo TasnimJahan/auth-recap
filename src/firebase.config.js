09395a38577092acb1cad14b6fbc67133627a72b
Sultana Tasnim Jahan <tasnim047sultana@gmail.com>
2021-03-17 22:55:36 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDRVgU2ghfyk08HOSOU3fXbURfLce73qXQ",
+    authDomain: "auth-recap111.firebaseapp.com",
+    projectId: "auth-recap111",
+    storageBucket: "auth-recap111.appspot.com",
+    messagingSenderId: "109355752355",
+    appId: "1:109355752355:web:81fb7138b3ddef32fc5816"
+  };
+  export default firebaseConfig;
